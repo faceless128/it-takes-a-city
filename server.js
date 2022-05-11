6499867c647e1978dd9dbc6ea1810e0975f3bcef
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-05-11 18:28:48 -0400

    added in start of requirements for the server.js file

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-// TO DO:
-// REQUIREMENTS //
-// Path
-// Express
-// Express Session
-// Handlebars
+// REQUIREMENTS START //
+
+// Require Path
+const path = require("path");
+
+// Require Express
+const express = require("express");
+
+// Require Express Session
+const session = require("express-session");
+
+// Require Handlebars
+const exphbs = require("express-handlebars");
 
 // Set up Helpers - Optional
+const helpers = require("./utils/helpers");
+
+// Set up App
+const app = express();
 
 // Set up PORT
+
+// Set up requirement with the connection.js file we created
+const PORT = process.env.PORT || 3001;
+
+// Require Sequelize
+const sequelize = require("./config/connection");
+
+// Require Connect Session Sequelize 
 
 // Const sess
 
