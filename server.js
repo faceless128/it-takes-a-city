15e90d5973e96312e0cd1e3206d49590a9ac995a
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-05-11 18:39:47 -0400

    added remaining details to run the server in server.js

--- server.js
+++ server.js
@@ -26,12 +26,50 @@ const PORT = process.env.PORT || 3001;
 // Require Sequelize
 const sequelize = require("./config/connection");
 
-// Require Connect Session Sequelize 
+// Require Connect Session Sequelize
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-// Const sess
-
+// Set up Session
+const sess = {
+  // Set up Secret Name
+  secret: "Ultra Top Secret",
+  // Set up Cookie
+  cookie: {},
+  // Set up Resave
+  resave: false,
+  // Set up saveUninitalized to true
+  saveUninitalized: true,
+  // Set up way to store via sequelize
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 // App Use
+app.use(session(sess));
+
+// Create Helpers - OPTIONAL
+const hbs = exphbs.create({ helpers });
+
+// Set up engine
+app.engine("handlebars", hbs.engine);
+// Set up view engine
+app.set("view engine", "handlebars");
+
+// Set up App USE #1 - Express.json()
+app.use(express.json());
+
+// Set up App USE #2 - Urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// Set up App USE #3 - sets up "public" file with stylesheets and Javascript to static resources
+app.use(express.static(path.join(__dirname, "public")));
+
+// Set up App USE #4 - Controllers
+app.use(require("./controllers/"));
 
 // Sequelize Sync
-
-// App listen
+sequelize.sync({ force: false }).then(() => {
+  // Sets up app listen
+  // With a rocket emoji because ✨ I can ✨
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}! 🚀`));
+});
