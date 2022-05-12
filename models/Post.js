3db5f6529b6f1560fe2babf4a7c95224be03add7
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 08:45:59 -0600

    Post Model created

--- models/Post.js
+++ models/Post.js
@@ -1,19 +1,42 @@
-// REQUIREMENTS START //
-
-// Require Model and DataType
 const { Model, DataTypes } = require("sequelize");
-
-// Require Sequelize
 const sequelize = require("../config/connection");
 
-// REQUIREMENTS END // 
-
-// Create Model
 class Post extends Model {}
 
-// Create fields/columns here
+// this is the Post model
 Post.init({
-  // Columns will go here!
-});
-// Export Model
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [1]
+    }
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [1]
+    }
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "user",
+      key: "id"
+    }
+  }
+}, {
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "post"
+})
+
 module.exports = Post;
