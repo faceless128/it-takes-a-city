3919852ce98ad25fe851011e67ff2f85b416d949
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 08:16:40 -0600

    foodbank route requirements added

---
+++ controllers/api/foodbank-routes.js
@@ -0,0 +1,10 @@
+const router = require("express");
+const {
+  User,
+  Post,
+  Comment,
+  City,
+  Foodbank,
+} = require("../../models");
+const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
