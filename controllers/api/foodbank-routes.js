826d3e1e114e27b8ce32ceee4b5fdd67912ed56f
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 08:19:07 -0600

    GET all foodbanks route added

--- controllers/api/foodbank-routes.js
+++ controllers/api/foodbank-routes.js
@@ -8,3 +8,13 @@ const {
 } = require("../../models");
 const sequelize = require("../../config/connection");
 const withAuth = require("../../utils/auth");
+
+// this route will GET all foodbanks
+router.get("/", (req, res) => {
+  Foodbank.findAll()
+  .then((dbFoodbankData) => res.json(dbFoodbankData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
