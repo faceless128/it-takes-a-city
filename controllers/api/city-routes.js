1fa2a002d49132b6a2df0a342f434eb60c714aa7
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 07:58:19 -0600

    GET one city by id added

--- controllers/api/city-routes.js
+++ controllers/api/city-routes.js
@@ -16,3 +16,29 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// this route will GET one city by id
+router.get("/:id", (req, res) => {
+  City.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [{
+      model: Foodbank,
+      attributes: ["id", "foodbank_name", "address"]
+    }]
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({
+        message: "No city found with this id."
+      });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
