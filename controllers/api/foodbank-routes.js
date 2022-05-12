80efd0d3890b81dead5bac001a608cd75dd42a3d
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 08:25:26 -0600

    GET foodbank by id route added

--- controllers/api/foodbank-routes.js
+++ controllers/api/foodbank-routes.js
@@ -12,9 +12,36 @@ const withAuth = require("../../utils/auth");
 // this route will GET all foodbanks
 router.get("/", (req, res) => {
   Foodbank.findAll()
-  .then((dbFoodbankData) => res.json(dbFoodbankData))
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbFoodbankData) => res.json(dbFoodbankData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// this route will GET a foodbank by id
+router.get("/:id", (req, res) => {
+  Foodbank.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "foodbank_name", "address"],
+      include: [{
+        model: City,
+        attributes: ["name", "stateName"],
+      }]
+    })
+    .then((dbFoodbankData) => {
+      if (!dbFoodbankData) {
+        res.status(404).json({
+          message: "No foodbank found with this id."
+        });
+        return;
+      }
+      res.json(dbFoodbankData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
