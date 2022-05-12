16771b0315e88632dcf867b621a7df7b9f0c685e
Kaijam Jeffers <faceless128@gmail.com>
2022-05-12 11:35:37 -0400

    route to show all users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -10,4 +10,18 @@ const sequelize = require("../../config/connection");
 const withAuth = require("../../utils/auth");
 
 
+// GET request to show all users
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method
+    User.findAll({
+        attributes: { exclude: ['password'] }
+        // excludes password field
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
