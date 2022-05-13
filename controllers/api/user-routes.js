833c3ea0b415ac6fd3b91f43dc2fb10a79f0c36c
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 18:17:25 -0600

    PUT route to update users added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -130,4 +130,27 @@ router.post("/logout", (req, res) => {
   }
 });
 
+// this route is to update (PUT) a user by id
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({
+          message: "No user found with this id!"
+        });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
