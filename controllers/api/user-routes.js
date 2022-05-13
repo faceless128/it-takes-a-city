3139f41ea27df78c74cb6d586da6cde2b5bf6b4c
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 18:14:45 -0600

    POST/create a user route added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -61,4 +61,25 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// this route will POST/create a user
+router.post("/", (req, res) => {
+  User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
