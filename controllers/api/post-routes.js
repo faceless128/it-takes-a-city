46290bb223895fd1b96c2579307f5d950a96b650
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 18:09:54 -0600

    PUT/update route for posts addded

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -87,3 +87,28 @@ router.post("/", withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// this route is for users to PUT/update a post by id
+router.put("/:id", withAuth, (req, res) => {
+  Post.update({
+      title: req.body.title,
+      content: req.body.post_content,
+    }, {
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: "No post found with this id!"
+        });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
