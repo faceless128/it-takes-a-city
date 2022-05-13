16753e56355a41c3b9aa3ba135ab979cf8836efe
Kaijam Jeffers <faceless128@gmail.com>
2022-05-12 20:13:26 -0400

    added route for single user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -24,4 +24,25 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+        // Includes will go here
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
