65b8b91d99759e0dd19130f134e7bcd0e2011ed5
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 18:40:35 -0600

    Add GET route to dashboard to get a new post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -50,43 +50,52 @@ router.get("/", withAuth, (req, res) => {
 // this route will GET posts by id
 // users will be required to be logged in to use this feature
 router.get("/edit/:id", withAuth, (req, res) => {
-      Post.findOne({
-          where: {
-            id: req.params.id
-          },
-          attributes: ["id", "title", "content", "created_at"],
-          include: [{
-              model: Comment,
-              attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-              include: {
-                model: User,
-                attributes: ["username"]
-              }
-            },
-            {
-              model: User,
-              attributes: ["username"]
-            }
-          ]
-        })
-        .then(dbPostData => {
-            if (!dbPostData) {
-                res.status(404).json({
-                  message: "No post found with this id!"
-                });
-                return;
-              }
-              const post = dbPostData.get({
-                plain: true
-              }); res.render("edit-post", {
-                post,
-                loggedIn: true
-              });
-            })
-          .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-          });
+  Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ["id", "title", "content", "created_at"],
+      include: [{
+          model: Comment,
+          attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"]
+          }
+        },
+        {
+          model: User,
+          attributes: ["username"]
+        }
+      ]
+    })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: "No post found with this id!"
         });
+        return;
+      }
+      const post = dbPostData.get({
+        plain: true
+      });
+      res.render("edit-post", {
+        post,
+        loggedIn: true
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// this route is to GET a new post
+router.get("/new", (req, res) => {
+  res.render("add-post", {
+    loggedIn: true
+  })
+})
 
 // EXPORT MODULE //
+module.exports = router;
