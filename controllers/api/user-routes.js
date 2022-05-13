33494b6678b894edffa84537e1fa80ff7969ffa0
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 18:12:40 -0600

    GET all users route added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,10 +4,23 @@ const {
   Post,
   Comment,
   City,
-  Foodbank,
+  Location,
 } = require("../../models");
 const sequelize = require("../../config/connection");
 const withAuth = require("../../utils/auth");
 
+// this route is to GET all users
+router.get("/", (req, res) => {
+  User.findAll({
+      attributes: {
+        exclude: ["password"]
+      }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
