5a8645fb5bf3720a90b4d11d27025e91bc092a47
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 18:18:09 -0600

    DELETE user by id route added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -153,4 +153,26 @@ router.put("/:id", (req, res) => {
     });
 });
 
+// this route is to DELETE a user by id
+router.delete(":/id", (req, res) => {
+  User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(dbUserData => {
+      if (!dbUserdata) {
+        res.status(404).json({
+          message: "No user found with this id!"
+        });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
