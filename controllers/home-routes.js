9522bdce4ab2e111d45b4c57f57c58520d7c1ffb
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 18:27:40 -0600

    GET all posts for our homepage route add

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,4 +11,37 @@ const {
 
 // ROUTES //
 
+// this route will GET all posts for our homepage
+router.get("/", (req, res) => {
+  Post.findAll({
+      attributes: ["id", "title", "content", "created_at"],
+      include: [{
+          model: Comment,
+          attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"]
+          }
+        },
+        {
+          model: User,
+          attributes: ["username"]
+        }
+      ]
+    })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({
+        plain: true
+      }));
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 // EXPORT MODULE //
