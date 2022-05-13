4df00e49a34fc7cc5df31cdb05c990a3d0a80c10
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-05-13 11:40:12 -0400

    added psuedocode for remaining functionality in index.js in seed folder

--- seeds/index.js
+++ seeds/index.js
@@ -13,3 +13,31 @@ const seedCity = require("./city-seeds");
 
 // Require Sequelize
 const sequelize = require("../config/connection");
+
+// Set up Seed ALL function
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  // Start up seed file for Cities
+
+  // END //
+
+  // Start up seed file for Comments
+
+  // END //
+
+  // Start up seed file for Location
+
+  // END //
+
+  // Start up seed file for LocationTag
+
+  // END //
+
+  // Start up seed file for Post
+
+  // END //
+
+  // Start up seed file for User
+
+  // END //
+};
