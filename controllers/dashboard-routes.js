8913579ee3869f7b71665e03fe3346e7e87888f0
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 18:36:46 -0600

    GET all posts for dashboard route add

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -10,8 +10,42 @@ const withAuth = require('../utils/auth');
 
 // ROUTES //
 
-// Route #1 - Get ALL Posts/Resources //
+// this route will GET all posts for the dashboard
+// users will be required to be logged in to use this feature
+router.get("/", withAuth, (req, res) => {
+  Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      attributes: ["id", "title", "content", "created_at"],
+      include: [{
+          model: Comment,
+          attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"]
+          }
+        },
+        {
+          model: User,
+          attributes: ["username"]
+        }
+      ]
+    })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({
+        plain: true
+      }));
+      res.render("dashboard", {
+        posts,
+        loggedIn: true
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-// Route #2 - Get Post with ID to Edit //
 
 // EXPORT MODULE //
