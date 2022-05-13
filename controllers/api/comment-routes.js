a6c7f26359a180328db55473cc5bd802a416c6ff
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-12 18:19:58 -0600

    GET route to find all comments route added

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -7,3 +7,13 @@ const {
   Location
 } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+// this route will GET all comments
+router.get("/", (req, res) => {
+  Comment.findAll()
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
