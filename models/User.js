9b93209ef5a0bd29f47ed7da3c491bac3aa641e4
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-05-14 19:52:16 -0400

    added in add-post.js logic

--- models/User.js
+++ models/User.js
@@ -1,47 +1,30 @@
-const {
-  Model,
-  DataTypes
-} = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+class User extends Model {}
 
 // this is the User model
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
- 
-}, {
-  hooks: {
-    // set up beforeCreate lifecycle "hook" functionality
-    async beforeCreate(newUserData) {
-      newUserData.password = await bcrypt.hash(newUserData.password, 10);
-      return newUserData;
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-
-    async beforeUpdate(updatedUserData) {
-      updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-      return updatedUserData;
-    }
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  sequelize,
-  timestamps: false,
-  freezeTableName: true,
-  underscored: true,
-  modelName: "user"
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
 
 module.exports = User;
--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,1 +1,36 @@
-// TO DO: We would create the add post logic here 
+async function newFormHandler(event) {
+  // Prevents default from happening
+  event.preventDefault();
+
+  // Sets title to query selector with post-title
+  const title = document.querySelector("").value;
+
+  // Sets post_url to query selector with post-url
+  const post_data = document.querySelector("").value;
+
+  // Response for POST
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      // Add in the user_id - we need something in our database table
+      user_id,
+      post_data,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  // If Response is OK - do the following
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+// Event Listener for New Post Form
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
