09dcf43011094302e54c3ff6a81a8e5ab76c477d
Kaijam Jeffers <faceless128@gmail.com>
2022-05-18 20:52:34 -0400

    comments js and form created, currently hardcoded to post #6

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,1 +1,34 @@
-// TO DO: Adding a comment logic would go here 
+async function newFormHandler(event) {
+    // Prevents default from happening
+    event.preventDefault();
+  
+    // Sets title to query selector with post-title
+    const comment_text = document.querySelector('input[name="comment-text"]').value;
+  
+    const post_id = 6;
+  
+    // Response for POST
+    const response = await fetch(`/api/comments`, {
+      method: "POST",
+      body: JSON.stringify({
+        comment_text,
+        post_id
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    // If Response is OK - do the following
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  // Event Listener for New Post Form
+  document
+    .querySelector(".add-comment-form")
+    .addEventListener("submit", newFormHandler);
+  
