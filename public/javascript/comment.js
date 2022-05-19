f8b01b031aa703fc64b9123555b6d26c05a09d29
Kaijam Jeffers <faceless128@gmail.com>
2022-05-18 21:02:42 -0400

    merge resolution

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-// function for submission of a new comment
-// capture the form content
-  async function commentFormHandler(event) {
-  event.preventDefault();
-
-  const comment_text = document.querySelector("").value.trim();
-
-  const post_id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ];
-
-  // defining the method as a POST
-  if (comment_text) {
-    const response = await fetch("/api/comments", {
-      method: "POST",
-      body: JSON.stringify({
-        post_id,
-        comment_text
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-
-    if (response.ok) {
-      document.location.reload();
-=======
 async function newFormHandler(event) {
     // Prevents default from happening
     event.preventDefault();
@@ -50,21 +22,13 @@ async function newFormHandler(event) {
     // If Response is OK - do the following
     if (response.ok) {
       document.location.replace("/profile");
->>>>>>> feature/profile
     } else {
       alert(response.statusText);
     }
   }
-<<<<<<< HEAD
-}
-
-// this is event listener for submission of a new comment
-document.querySelector("").addEventListener("submit", commentFormHandler);
-=======
   
   // Event Listener for New Post Form
   document
     .querySelector(".add-comment-form")
     .addEventListener("submit", newFormHandler);
   
->>>>>>> feature/profile
